ed47ebe5e7978c897750194edb4e4235b42b344e
momtazrasel <momtazrasel8@gmail.com>
2025-07-07 23:13:04 +0600

    Test file and Readme file are updated

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # API_Test_Using_Jest
+
+## Initialize Project and install dependency
+    npm init -y
+    npm install jest axios dotenv
+
+## Run Tests
+    npm test
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL || "https://your-api-url.com/api", // change this
+  baseURL: process.env.BASE_URL , // change this
   headers: {
     "Content-Type": "application/json"
   },
